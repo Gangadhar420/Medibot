d6ab0c87d052b22c71406112af97ed7954b534de
Gangadhar <gangadhar.kota@machint.com>
2025-10-31 15:16:42 +0530

    project structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating directories
+mkdir -p src
+mkdir -p research
+
+# creating files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch requirements.txt
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch README.md
+
+echo "Project structure created successfully."
